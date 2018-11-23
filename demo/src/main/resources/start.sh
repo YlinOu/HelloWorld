a851dd4a2a153b29bd9d0300eb9f336b44059ae5
Ylin <ylin_ou2@outlook.com>
2018-11-23 17:05:25 +0800

    Fixed

--- demo/src/main/resources/start.sh
+++ demo/src/main/resources/start.sh
@@ -1,2 +1,3 @@
 ps -ef | grep demo | grep -v grep | awk '{print $2}'|xargs --no-run-if-empty kill -9
+sleep 1
 nohup java -jar /var/lib/jenkins/workspace/test/demo/target/demo-0.0.1-SNAPSHOT.jar >/root/null 2>&1 &

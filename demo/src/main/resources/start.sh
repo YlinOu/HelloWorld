de5822573987d44dfcfd07f956ca0dfe6ff7173b
Ylin <ylin_ou2@outlook.com>
2018-11-23 17:07:39 +0800

    test

--- demo/src/main/resources/start.sh
+++ demo/src/main/resources/start.sh
@@ -1,3 +1,4 @@
+chmod u+x *.sh
 ps -ef | grep demo | grep -v grep | awk '{print $2}'|xargs --no-run-if-empty kill -9
 sleep 1
 nohup java -jar /var/lib/jenkins/workspace/test/demo/target/demo-0.0.1-SNAPSHOT.jar >/root/null 2>&1 &
